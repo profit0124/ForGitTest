77964bd56310c91e07ed5a53d55fe19674b100dc
profit0124 <profit0124@gmail.com>
2023-04-09 22:16:44 +0900

    Initial Commit

---
+++ ForGitTest/ForGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGitTestApp.swift
+//  ForGitTest
+//
+//  Created by Sooik Kim on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct ForGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
